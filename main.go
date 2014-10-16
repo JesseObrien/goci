77bc0b9a84eef4e25e10b97d4d9ebc5fa62150c6
Jesse O'Brien <jesseobrien@rtraction.com>
2014-10-16 17:15:26 -0400

    Massive updates

--- main.go
+++ main.go
@@ -5,49 +5,33 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/phyber/negroni-gzip/gzip"
-	"github.com/xyproto/permissions"
-	"gopkg.in/unrolled/render.v1"
-	"net/http"
 )
 
 var (
 	debugMode = flag.Bool("debug", false, "Debug switch.")
+	port      = flag.String("port", ":8080", "Server port")
 )
 
 func init() {
 	flag.Parse()
 }
 
-func getUserState() *permissions.UserState {
-	perm := permissions.New()
-
-	return perm.UserState()
-}
-
 func main() {
-	userstate := getUserState()
 
-	authHandler := AuthHandler{userstate: userstate}
+	routes := NewRoutes()
 
 	mux := mux.NewRouter()
 
-	r := render.New(render.Options{})
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		if userstate.IsLoggedIn(userstate.GetUsername(req)) {
-			r.HTML(w, http.StatusOK, "dashboard", nil)
-		} else {
-			r.HTML(w, http.StatusOK, "landing", nil)
-		}
-	})
-
-	mux.HandleFunc("/register", authHandler.Register).Methods("POST")
-	mux.HandleFunc("/login", authHandler.Login).Methods("POST")
-	mux.HandleFunc("/logout", authHandler.Logout).Methods("GET")
+	mux.HandleFunc("/", routes.GetIndex).Methods("GET")
+	mux.HandleFunc("/register", routes.GetRegister).Methods("GET")
+	mux.HandleFunc("/register", routes.PostRegister).Methods("POST")
+	mux.HandleFunc("/login", routes.Login).Methods("POST")
+	mux.HandleFunc("/logout", routes.Logout).Methods("GET")
+	mux.HandleFunc("/builds", routes.Builds).Methods("GET")
 
 	n := negroni.Classic()
 	n.Use(gzip.Gzip(gzip.DefaultCompression))
 	n.UseHandler(mux)
-	n.Run(":8080")
+	n.Run(":8888")
 
 }
---
+++ routes.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"github.com/xyproto/permissions"
+	"gopkg.in/unrolled/render.v1"
+	"net/http"
+)
+
+type Routes struct {
+	userstate permissions.UserState
+	renderer  render.Render
+}
+
+func (r *Routes) LayoutRender(w http.ResponseWriter, status int, name string, binding interface{}, htmlOpt ...render.HTMLOptions) {
+
+	htmlOpt = append(htmlOpt, render.HTMLOptions{Layout: "layout"})
+
+	r.renderer.HTML(w, status, name, binding, htmlOpt...)
+}
+
+func (r *Routes) Render(w http.ResponseWriter, status int, name string, binding interface{}, htmlOpt ...render.HTMLOptions) {
+	r.renderer.HTML(w, status, name, binding, htmlOpt...)
+}
+
+func NewRoutes() *Routes {
+
+	r := render.New(render.Options{})
+	perm := permissions.New()
+	us := perm.UserState()
+
+	return &Routes{userstate: *us, renderer: *r}
+}
+
+func (r *Routes) GetIndex(w http.ResponseWriter, req *http.Request) {
+	r.Render(w, http.StatusOK, "landing", nil)
+}
+
+func (r *Routes) GetRegister(w http.ResponseWriter, req *http.Request) {
+	r.LayoutRender(w, http.StatusOK, "registration", nil)
+}
+
+func (r *Routes) PostRegister(w http.ResponseWriter, req *http.Request) {
+
+	username := req.FormValue("username")
+	email := req.FormValue("email")
+	password := req.FormValue("password")
+
+	if ok := r.userstate.HasUser(username); ok != false {
+		http.Redirect(w, req, "/register", http.StatusFound)
+	}
+
+	if ok := permissions.ValidUsernamePassword(username, password); ok != nil {
+
+	}
+
+	r.userstate.AddUser(username, password, email)
+
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
+func (r *Routes) Login(w http.ResponseWriter, req *http.Request) {
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	if ok := r.userstate.CorrectPassword(username, password); ok != false {
+
+	}
+
+	r.userstate.Login(w, username)
+
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
+func (r *Routes) Logout(w http.ResponseWriter, req *http.Request) {
+	r.userstate.Logout(r.userstate.GetUsername(req))
+
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
+func (r *Routes) Builds(w http.ResponseWriter, req *http.Request) {
+	data := struct {
+		Pagename string
+	}{
+		"Builds",
+	}
+	r.LayoutRender(w, http.StatusOK, "builds", data)
+}
